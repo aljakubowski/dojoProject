9fa76f45488961da962256dbab1a279f6a265f7d
Alfred Jakubowski <al.jakubowski@icloud.com>
2022-04-21 09:19:41 +0200

    m1

--- src/main/java/com/dojo1/dojobf/controller/RickMortyApiController.java
+++ src/main/java/com/dojo1/dojobf/controller/RickMortyApiController.java
@@ -5,6 +5,7 @@ import com.dojo1.dojobf.service.RickMortyApiService;
 import com.dojo1.dojobf.webclient.rickymortyapi.dto.EpisodesResultsDTO;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +31,7 @@ public class RickMortyApiController {
     }
 
     @GetMapping(path = "/episodes")
+    @CrossOrigin(origins = "http://localhost:4200")
     public RickMortySeasonsEpisodesDTO getSeasonAndEpisodes(){
         return rmApiService.getSeasonAndEpisodes();
     }
